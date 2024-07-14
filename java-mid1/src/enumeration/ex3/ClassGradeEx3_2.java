684f308bd66180f84927c36e6f9fd83fbf40456b
YeonjiIsGonji <165933276+YeonjiIsGonji@users.noreply.github.com>
2024-07-14 21:00:46 +0900

    자바중급강의_열거형ENUM 예제풀이

---
+++ java-mid1/src/enumeration/ex3/ClassGradeEx3_2.java
@@ -0,0 +1,17 @@
+package enumeration.ex3;
+
+import enumeration.ex2.ClassGrade;
+import enumeration.ex2.DiscountService;
+
+public class ClassGradeEx3_2 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+//
+//        Grade newClassGrade = new Grade(); // 생성자 private으로 막아야 함
+//        int result = discountService.discount(Grade, price);
+//        System.out.println("result price: " + result);
+    }
+}
